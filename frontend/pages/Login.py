172b655f670f1bdcef29af7cae30e3c6b7a64b21
Syed Kaab Surkhi <86132908+SurkhiSyed@users.noreply.github.com>
2025-01-02 23:17:20 -0500

    fixed sign in stuff

--- frontend/pages/Login.py
+++ frontend/pages/Login.py
@@ -3,10 +3,25 @@ import firebase_admin
 from firebase_admin import credentials, auth
 
 # Check if the default app is already initialized
+
 if not firebase_admin._apps:
-    cred = credentials.Certificate('pages/bete-6f08c-1ea6fff3125d.json')
+    cred = credentials.Certificate('../beted-83806-firebase-adminsdk-1k71h-c002aa1ad7.json')
     firebase_admin.initialize_app(cred)
 
+
+st.sidebar.title('BetEd')
+
+firebaseConfig = {
+  'apiKey': "AIzaSyC1tQ8iuQkGsgtpFZsNDkcMp9uruvPHuGc",
+  'authDomain': "beted-83806.firebaseapp.com",
+  'projectId': "beted-83806",
+  'storageBucket': "beted-83806.firebasestorage.app",
+  'messagingSenderId': "758279482712",
+  'appId': "1:758279482712:web:f5d1aed199bf88e548092d",
+  'measurementId': "G-5J78FB98NS"
+}
+
+#Authetication
 def app():
     st.title('Login')
     choice = st.selectbox("Login/Signup", ["Login", "Sign Up"])
@@ -32,7 +47,7 @@ def app():
             except:
                 st.warning("Login Failed. User not found.")
     else:
-        email = st.text_input("Email Address")
+        email = st.text_input("Email Address", value='Default')
         password = st.text_input("Password", type="password")
         username = st.text_input("Enter your unique username")
         
